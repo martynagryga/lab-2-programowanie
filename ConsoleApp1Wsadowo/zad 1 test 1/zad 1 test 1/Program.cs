c5ef286220a46ce4e12aa25330cb4881930ff289
martynagryga <martyna.gryga@microsoft.wsei.edu.pl>
2021-04-01 08:21:45 +0200

    cw

---
+++ ConsoleApp1Wsadowo/zad 1 test 1/zad 1 test 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_1_test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    if (i != j)
+                    {
+                        n--;
+                    }
+                }
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
