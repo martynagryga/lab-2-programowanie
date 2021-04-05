e04763798ca7d466cf41c71fd420ce236a3d8dc2
martynagryga <martyna.gryga@microsoft.wsei.edu.pl>
2021-04-05 16:39:37 +0200

    spoj

---
+++ ConsoleApp1Wsadowo/cwiczenia spoj/cwiczenia spoj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cwiczenia_spoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
