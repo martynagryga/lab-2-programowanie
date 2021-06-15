27ad48a06316ee8697042930f0a56a41adc4fa07
martynagryga <martyna.gryga@microsoft.wsei.edu.pl>
2021-06-15 12:20:58 +0200

    cw

---
+++ ConsoleApp1Wsadowo/Zadanie klasa Person, Child/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_klasa_Person__Child
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
